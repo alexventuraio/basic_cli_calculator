62801759a0928f0d45e8e648d4d70b114e029ed6
Alejandro Ventura Contreras <soyalexventura@gmail.com>
2020-06-26 18:05:41 -0500

    Add handler for numbers. - Create two new instance variables to store the next value that the user is typing and the value to be displayed in the calculator. - Now show what the user types in the calculator display. - Verify that the user input is a valid number with a regexp. - If the entry is valid, make sure to cast positive numbers without the '+' symbol. - Prevent from having entries like '33-1' as a valid input.

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -6,11 +6,13 @@ class Calculator
 
   def initialize
     @calc = CoreCalculator.new
+    @next_value = ''
+    @display_value = @calc.result
   end
 
   def call
     render_view_welcome_msg
-    render_view_menu(0)
+    render_view_menu(@calc.result)
     keep_alive
   end
 
@@ -19,6 +21,8 @@ class Calculator
       entry.downcase!
 
       case
+      when is_a_number?(entry)
+        process_number(entry)
       when entry.eql?('c')
         @calc.reset
       when entry.eql?('q')
@@ -28,11 +32,31 @@ class Calculator
         render_view_exception("Unknown command: '#{entry}'")
       end
 
-      render_view_menu(0)
+      render_view_menu(@display_value)
     end
   end
 
   private
+
+  def process_number(entry)
+    # Make sure to cast entries like '+5' into '5'
+    entry.sub!('+', '')
+    # Make sure not to have string like '33-1' as a whole
+    valid_group = Float(@next_value + entry) rescue nil
+
+    if valid_group
+      @next_value << entry
+    else
+      # Raise an exception
+      render_view_exception('Wrong numeric format!')
+    end
+
+    @display_value = @next_value
+  end
+
+  def is_a_number?(value)
+    value =~ /\A[-+]?[0-9]*\.?[0-9]+\Z/
+  end
 
   def get_user_input
     gets.chomp
