5b36b249e3640f69f552ea1dc2daeac6929015d0
Alejandro Ventura Contreras <soyalexventura@gmail.com>
2020-06-27 13:22:09 -0500

    Use the new helper to simulate user entries as in the command line and perform operations.

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,4 +1,9 @@
 require_relative '../lib/calculator'
+require_relative 'support/io_test_helpers'
+
+RSpec.configure do |c|
+  c.include IoTestHelpers
+end
 
 describe Calculator do
   describe 'initialization' do
@@ -13,6 +18,32 @@ describe Calculator do
 
     it 'has a responds to call method' do
       expect(subject).to respond_to :call
+    end
+  end
+
+  describe 'basic arithmetic operations' do
+    it 'should follow the process and return 10.0' do
+      expect {
+        simulate_stdin('3', '+', '2', '*', '2', '=', 'q') do
+          described_class.new(silent_mode: true).call
+        end
+      }.to output("10.0\n").to_stdout
+    end
+
+    it 'should follow the process and return 139.6' do
+      expect {
+        simulate_stdin('700', '+', '-2', '/', '5', '=', 'q') do
+          described_class.new(silent_mode: true).call
+        end
+      }.to output("139.6\n").to_stdout
+    end
+
+    it 'should follow the process and return `Infinity`' do
+      expect {
+        simulate_stdin('50', '/', '0', '=', 'q') do
+          described_class.new(silent_mode: true).call
+        end
+      }.to output("Infinity\n").to_stdout
     end
   end
 end
